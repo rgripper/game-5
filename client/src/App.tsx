c0aa5c4c8b1282fc36d53751cd5799bb7f9fd31b
Vladimir Zotov <azuresplash@gmail.com>
2019-03-06 07:48:20 +1100

    Loaded sprites

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,29 @@
 import React, { Component, useEffect } from 'react';
 import * as PIXI from 'pixi.js';
 import './App.css';
-
+import mosterImage from './assets/Monster.png';
+import playerImage from './assets/Player.png';
 function App () {
 
     useEffect(() => {
-      var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+      const app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
       document.getElementById('app')!.appendChild(app.view);
+      const monster = PIXI.Sprite.from(mosterImage);
+      const player = PIXI.Sprite.from(playerImage);
+      monster.anchor.set(0.5);
+
+      // move the sprite to the center of the screen
+      monster.x = app.screen.width / 2;
+      monster.y = app.screen.height / 2;
+
+      app.stage.addChild(monster);
+
+      app.ticker.add(function(delta) {
+        // just for fun, let's rotate mr rabbit a little
+        // delta is 1 if running at 100% performance
+        // creates frame-independent transformation
+        monster.rotation += 0.1 * delta;
+    });
     })
 
     return (
